51d0c2d4f30deec106d8ba371a54966f373511db
Murtaza Munshi <murtaza.munshi@ekomercio.com.mx>
2020-03-14 09:52:24 +0530

    added preact and moved react to dev-dep

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 const withImages = require('next-images');
-//module.exports = withImages()
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withImages())
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS(withImages({
+  webpack: function (config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: 'preact/compat',
+      react$: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom$': 'preact/compat',
+    }
+
+    return config
+  },
+}));
